de18b4a2a7479c8e71aef0e36c8620acfd15376f
Sagnik Dey <sagnikdey@SUVADIPs-MacBook-Pro.local>
2020-12-04 16:46:38 +0800

    Updates

---
+++ precache-manifest.eca221c742bc38caf8d23e2d59515e72.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dd0d75f601ea077c2c4805d900e9467",
+    "url": "/restwithdignity/index.html"
+  },
+  {
+    "revision": "0658cf6262d97a4e97eb",
+    "url": "/restwithdignity/static/css/2.48e152d1.chunk.css"
+  },
+  {
+    "revision": "704a8f789306b3138811",
+    "url": "/restwithdignity/static/css/main.cb4ef030.chunk.css"
+  },
+  {
+    "revision": "0658cf6262d97a4e97eb",
+    "url": "/restwithdignity/static/js/2.69898e7c.chunk.js"
+  },
+  {
+    "revision": "704a8f789306b3138811",
+    "url": "/restwithdignity/static/js/main.a626d758.chunk.js"
+  },
+  {
+    "revision": "216a661b45d94b39bd95",
+    "url": "/restwithdignity/static/js/runtime~main.12f4d8ec.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/restwithdignity/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/restwithdignity/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/restwithdignity/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/restwithdignity/static/media/slick.f97e3bbf.svg"
+  }
+]);
